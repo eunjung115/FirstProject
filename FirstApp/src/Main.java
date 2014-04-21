285f232f251a250ebaf54a32c07e55a1e466be0b
eunjung115 <eunjung115@naver.com>
2014-04-21 10:34:14 +0900

    gg

--- FirstApp/src/Main.java
+++ FirstApp/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Ã¹ ¹øÂ° ÇÁ·ÎÁ§Æ®");
+			System.out.println("Ã¹ ï¿½ï¿½Â° ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½zzÆ®");
 	}
 
 }
